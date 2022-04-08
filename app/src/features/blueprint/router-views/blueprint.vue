726f36b955deee1bfa567f1fd3341bc4af79f3e7
FrancoisBosma <francois.bosma@hotmail.fr>
2022-04-08 16:49:11 +0200

    feat: make the blueprint component use the full screen layout

--- app/src/features/blueprint/router-views/blueprint.vue
+++ app/src/features/blueprint/router-views/blueprint.vue
@@ -1,8 +1,15 @@
 <route lang="yaml">
 meta:
   public: true # cf. auth/navigationGuards.ts
+  layout: FullScreenLayout
 </route>
 
 <template>
-  <h1>XXXXXXX</h1>
+  <div class="blueprint-container"> </div>
 </template>
+
+<style scoped lang="postcss">
+  .blueprint-container {
+    @apply w-full h-full bg-red-500;
+  }
+</style>
