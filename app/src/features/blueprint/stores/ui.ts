c49acadb2daebcb2c8bdc31b421c9e0cf4322765
FrancoisBosma <francois.bosma@hotmail.fr>
2022-04-16 22:41:39 +0200

    feat: add a ui store

---
+++ app/src/features/blueprint/stores/ui.ts
@@ -0,0 +1,29 @@
+import { acceptHMRUpdate, defineStore } from 'pinia'
+import { objectMap } from '@GLOBAL/functions/objects'
+import type { ComputedRef, Ref } from 'vue'
+
+type Axis = string
+interface Dimension {
+  axis: Axis
+  boundingClientRectProperty: string
+  boxSizeProperty: string
+}
+
+export const useUiStore = defineStore('ui', () => {
+  // N.B: It is considered that the dimension names 'width' and 'height' are to remain forever unchanged.
+  //      Some code logic is thus based on it. Beware.
+  const dimensions: Ref<{ [x: string]: Dimension }> = ref({
+    width: { axis: 'x', boundingClientRectProperty: 'width', boxSizeProperty: 'inlineSize' },
+    height: { axis: 'y', boundingClientRectProperty: 'height', boxSizeProperty: 'blockSize' },
+  })
+  const axes: ComputedRef<{ [x: string]: Axis }> = computed(() =>
+    objectMap(dimensions.value, (dim: Dimension) => dim.axis)
+  )
+
+  return {
+    dimensions,
+    axes,
+  }
+})
+
+if (import.meta.hot) import.meta.hot.accept(acceptHMRUpdate(useUiStore, import.meta.hot))
