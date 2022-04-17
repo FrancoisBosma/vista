9da5691a505742551d9deccee26f85e37bbb293a
FrancoisBosma <francois.bosma@hotmail.fr>
2022-04-17 18:40:41 +0200

    feat: pass hover state to child

--- app/src/features/blueprint/components/Concept/CloseConcept/CloseConcept.vue
+++ app/src/features/blueprint/components/Concept/CloseConcept/CloseConcept.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-  defineProps<{ title: String; subTiles: Object[] }>()
+  defineProps<{ title: String; subTiles: Object[]; isHovered: Boolean }>()
 </script>
 
 <template>
   <h1>{{ title }}</h1>
+  <h1>{{ isHovered }}</h1>
 </template>
