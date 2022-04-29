8620afdd38a557a7941c5e50905b0f81d5d002f0
FrancoisBosma <francois.bosma@hotmail.fr>
2022-04-29 18:33:51 +0200

    fix: deactivate pinch behavior at the header level

--- app/src/features/blueprint/router-views/blueprint/index.vue
+++ app/src/features/blueprint/router-views/blueprint/index.vue
@@ -49,6 +49,15 @@ meta:
     getCurrentBiggestSquareLength,
     computeExtraOffset,
   })
+
+  useHead({
+    meta: [
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no',
+      },
+    ],
+  })
 </script>
 
 <template>
