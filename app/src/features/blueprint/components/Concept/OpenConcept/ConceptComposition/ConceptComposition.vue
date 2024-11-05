ebe169d4e0826bdd3ddc2a87b6e9b31d9f7845ab
FrancoisBosma <francois@groover.co>
2024-11-05 23:33:30 +0100

    feat(WIP, DrawnArrow): start using arrows for subConcept connections

--- app/src/features/blueprint/components/Concept/OpenConcept/ConceptComposition/ConceptComposition.vue
+++ app/src/features/blueprint/components/Concept/OpenConcept/ConceptComposition/ConceptComposition.vue
@@ -1,17 +1,36 @@
 <script setup lang="ts">
-  import { useUiStore } from '@FEATURES/blueprint/stores'
+  import { useConceptStore, useUiStore } from '@FEATURES/blueprint/stores'
   import type { Concept } from '@API/gql-generated/graphql'
+  import type { Pair } from '@ROOT/src/types'
 
   const { concept } = defineProps<{
     concept: Concept
   }>()
 
   const ui = useUiStore()
+  const conceptStore = useConceptStore()
 
   // Lazy loading necessary because of mutual nesting
   const ConceptSFC = defineAsyncComponent(() => import('@FEATURES/blueprint/components/Concept'))
 
   const contentEdgePositions = ui.getContentEdgePositions(concept)
+
+  // WIP
+  const connection = concept.composition?.connections[0]
+  const sourceXY = connection?.sourceCustomID
+  const targetXY = connection?.targetConceptCustomID
+  const sourceConceptName = connection?.sourceArgumentType.name.substring(
+    0,
+    connection?.sourceArgumentType.name.lastIndexOf('.')
+  )
+  const targetConceptName = concept.composition?.subConcepts.find((subConcept) => subConcept.xy === targetXY)?.concept
+    .name
+  const sourceWH = conceptStore.getStoreConcept(sourceConceptName ?? '')?.wh
+  const targetWH = conceptStore.getStoreConcept(targetConceptName ?? '')?.wh
+  const [sourceX, sourceY] = getNumbersFromPair(sourceXY as Pair<number>)
+  const [sourceW, sourceH] = getNumbersFromPair(sourceWH as Pair<number>)
+  const [targetX, targetY] = getNumbersFromPair(targetXY as Pair<number>)
+  const [targetW, targetH] = getNumbersFromPair(targetWH as Pair<number>)
 </script>
 
 <template>
@@ -20,5 +39,9 @@
     :key="index"
     :concept-name="subConcept.concept.name"
     :sub-concept-style="ui.getSubConceptStyle(contentEdgePositions, subConcept)"
+  />
+  <BoxToBoxArrow
+    :from="{ x: sourceX, y: sourceY, width: sourceW, height: sourceH }"
+    :to="{ x: targetX, y: targetY, width: targetW, height: targetH }"
   />
 </template>
